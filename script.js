9516babe90b78022c0bd515677be081df1817509
boulhouech <slim.boulahouech@gmail.com>
2024-02-09 12:40:01 +0100

    setting first ui and style it with css

---
+++ script.js
@@ -0,0 +1,8 @@
+const containerList = document.querySelector('.container');
+function createDiv () {
+    for (let i = 1; i < 16; i++) {
+        document.createElement('div');
+        containerList.appendChild()
+    }
+
+}
