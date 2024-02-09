800063c6720b76ac86da29138cd1a05e1e0bbc7b
boulhouech <slim.boulahouech@gmail.com>
2024-02-09 12:52:42 +0100

    finsihing selecting

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const blackColorButton = document.querySelector('.black');
 const rgbColorButton = document.querySelector('.rgb');
 const resetButton = document.querySelector('.reset');
+const getDimension = document.querySelector('.dimension');
+const boardGame = document.querySelector('.board');
+
+
 
