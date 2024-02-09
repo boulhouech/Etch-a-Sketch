08f3c84452fc0f4a545a3d7427b21cdf04bddcc3
boulhouech <slim.boulahouech@gmail.com>
2024-02-09 23:14:19 +0100

    restart with query selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+const getDimension = document.querySelector('input');
+const gameBoard = document.querySelector('.board');
+const blackColorButton = document.querySelector('.black');
+const rgbColorButton = document.querySelector('.rgb');
+const resetButton = document.querySelector('.reset');
+
+
