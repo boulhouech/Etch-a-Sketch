e1ed2b6bd72382bed9131275e8b84774e1235e2a
boulhouech <slim.boulahouech@gmail.com>
2024-02-10 13:24:41 +0100

    finaly figuring out how to set the grid

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const getDimension = document.querySelector('input');
 const gameBoard = document.querySelector('.board');
-const blackColorButton = document.querySelector('.black');
-const rgbColorButton = document.querySelector('.rgb');
-const resetButton = document.querySelector('.reset');
+const blackColorButton = document.querySelector('#black');
+const rgbColorButton = document.querySelector('#rgb');
+const resetButton = document.querySelector('#reset');
+
+function createGrid(rows, columns) {
+    rows = getDimension.value; 
+    columns = getDimension.value; 
 
 
+        // Clear the game board before creating a new grid
+        gameBoard.innerHTML = '';
+
+        // Create grid rows and columns
+        for (let i = 0; i < (rows * columns); i++) {
+            const gridCreated = document.createElement('div');
+            gridCreated.classList.add('grid-square');
+            gridCreated.style.width = `calc(100% / ${columns})`;
+            gridCreated.style.height = `calc(100% / ${rows})`;
+            gameBoard.appendChild(gridCreated); // Append the row to the game board
+        }
+    }
+
+    // Initial grid creation
+    createGrid(); 
